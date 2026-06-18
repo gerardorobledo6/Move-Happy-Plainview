b628799274df6f866121a6d9da8c89a02e3ed521
Gerardo <gerardo.robledo6@gmail.com>
2026-06-18 15:01:01 -0700

    Connect frontend to Render API

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://leankit-api.onrender.com/api',
 });
 
 client.interceptors.request.use((config) => {
